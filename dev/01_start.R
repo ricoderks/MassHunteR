83e9ae4431b2d8af4e100b6f4fea761e2f2a85e1
Rico Derks <r.j.e.derks@lumc.nl>
2021-12-13 07:55:15 +0100

    Starting project.

--- dev/01_start.R
+++ dev/01_start.R
@@ -35,7 +35,7 @@ golem::set_golem_options()
 ## See ?usethis for more information
 usethis::use_agpl3_license()  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
-usethis::use_code_of_conduct()
+# usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
 # usethis::use_news_md( open = FALSE )
 
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(MassHunteR)
+
+test_check("MassHunteR")
